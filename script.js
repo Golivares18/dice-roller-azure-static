bd2cac49578d015caf37070ba42bca4234144c5a
Greg Olivares <grolivares2002@gmail.com>
2025-03-02 19:00:06 -0600

    .

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ const serverUrl = "https://dice-roller-node-js-go-b5c8c5b0hahkcrfx.centralus-01.
 
 async function rollDice() {
     try {
-        // Call the API to roll a dice
         let response = await fetch(`${serverUrl}/roll-dice`);
-        
-        // Handle potential errors
+
         if (!response.ok) {
             throw new Error("Failed to fetch dice roll");
         }
 
         let data = await response.json();
-        
+
         // Display the rolled number
         document.getElementById("result").textContent = `You rolled a ${data.result}`;
     } catch (error) {
